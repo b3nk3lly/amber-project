de878d051ca455e5bfa0e5eeb4a6bbd95e3d0d39
b3nk3lly <benjkelly98@gmail.com>
2025-04-14 20:37:46 -0300

    Remove references to node icon

--- src/components/TreeNode.tsx
+++ src/components/TreeNode.tsx
@@ -3,16 +3,14 @@ import Modal from "./Modal";
 
 interface TreeNodeProps {
     children: React.ReactNode;
-    icon?: string;
     label?: string;
 }
 
-export default function TreeNode({ children, icon, label }: Readonly<TreeNodeProps>) {
+export default function TreeNode({ children, label }: Readonly<TreeNodeProps>) {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return <>
         <button className="bg-gray-200 hover:bg-white shadow-md rounded-full w-10 h-10 p-2 outline-2 hover:cursor-pointer flex justify-center items-center" onClick={() => setIsModalOpen(true)}>
-            {icon && <img src={icon} />}
             {label}
         </button>
         <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
